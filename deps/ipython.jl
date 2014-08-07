a7d4889bbd5441f0592730672266e5906f98b9ee
David Anthoff <anthoff@berkeley.edu>
2014-08-07 10:53:40 -0700

    Streamline ipython detection code

--- deps/ipython.jl
+++ deps/ipython.jl
@@ -2,31 +2,15 @@
 # IPython executable, and version is the VersionNumber.
 function find_ipython()
     if @windows? true : false
-        ipython = normpath(Pkg.dir("IJulia"),"deps", "usr", "python27", "scripts", "ipython.exe")
-        try
-            ipython,convert(VersionNumber, chomp(readall(`$ipython --version`)))
-        catch e5
-            error("IPython is required for IJulia, got errors\n",
-                "   $e5")
-        end
+        ipycmds = (normpath(Pkg.dir("IJulia"),"deps", "usr", "python27", "scripts", "ipython.exe"),)
     else
+        ipycmds = ("ipython", "ipython2", "ipython3")
+    end
+
+    for ipy in ipycmds
         try
-            "ipython",convert(VersionNumber, chomp(readall(`ipython --version`)))
-        catch e1
-            try
-                "ipython2",convert(VersionNumber, chomp(readall(`ipython2 --version`)))
-            catch e2
-                try
-                    "ipython3",convert(VersionNumber, chomp(readall(`ipython3 --version`)))
-                catch e3
-                    try
-                        "ipython.bat",convert(VersionNumber, chomp(readall(`ipython.bat --version`)))
-                    catch e4
-                        error("IPython is required for IJulia, got errors\n",
-                            "   $e1\n   $e2\n   $e3" * (@windows ? "\n$e4\n" : "") )
-                    end
-                end
-            end
+            return (ipy, convert(VersionNumber, chomp(readall(`$ipy --version`))))
         end
     end
+    error("IPython is required for IJulia")
 end
